9c011400eab411efc996fe691a12e77d3e21443a
JK <midhun.fegno@gmail.com>
2022-05-12 11:46:08 +0530

    settings changed added env files

--- fegtwitter/settings.py
+++ fegtwitter/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+env = os.environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d61zxnl+gd1kew_kgvqo!4&q(q&c-+!@6vi6kv_#&&jd1z)9+e'
+SECRET_KEY = env.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.get('DEBUG')
 
 ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = ['example.com','127.0.0.1']
@@ -83,10 +88,10 @@ WSGI_APPLICATION = 'fegtwitter.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'twitter_db',
-        'USER': 'marshal',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
+        'NAME': env.get('NAME'),
+        'USER': env.get('USER'),
+        'PASSWORD': env.get('PASSWORD'),
+        'HOST': env.get('HOST'),
         'POST': '',
     }
 }
