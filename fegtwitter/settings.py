72fda3b2e1cc920582c5369f80b7c212a304d3d0
JK <midhun.fegno@gmail.com>
2022-05-17 14:52:08 +0530

    before collect static

--- fegtwitter/settings.py
+++ fegtwitter/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_URL = 'assets/'
 # STATICFILES_DIRS = [ BASE_DIR / "public" / "assets", ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
